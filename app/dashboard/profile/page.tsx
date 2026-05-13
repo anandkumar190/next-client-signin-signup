cb8f3bfcefa0f924457efec55103c640f3618b54
Anand <anandkumar.kumar190@gmail.com>
2026-05-14 00:44:49 +0530

    profile api get data

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -2,11 +2,13 @@
 import Link from "next/link";
 import {useRouter} from "next/navigation";
 import axios from "axios";
+import constants from "constants";
+import React,{ useState } from "react";
 
 export default function profile(){
-
+       
         const router= useRouter();
-        
+        const [data ,setData]=useState("nothing");
 
     const onLogout = async ()=>{
 
@@ -20,7 +22,25 @@ export default function profile(){
         } catch (error:any) {
             console.log("error",error.message);
         }
-    }
+    };
+
+    const getUserDetails=async ()=>{
+        debugger;
+       
+       try {
+          const res = await axios.get("/api/users/profile");
+        console.log(res.data);
+        setData(res.data.data._id);
+       } catch (error:any) {
+
+        console.log(error.message);
+         console.log(error.response?.data)
+         console.log(error.response?.status)
+        
+       }
+      
+
+    };
     
    
 
@@ -30,12 +50,21 @@ export default function profile(){
     return(
         <>
             <h1>Profile</h1>
+            <h1>user</h1>
+            <h2>{data==="nothing"?"Nothing":<Link href={`/dashboard/profile/{$data}`}>{data}</Link>}</h2>
 
             <button
             type="button"
             onClick={onLogout}
             >
             Logout
+            </button>
+            <hr/>
+                     <button
+            type="button"
+            onClick={getUserDetails}
+            >
+            Get user data 
             </button>
 
         </>
