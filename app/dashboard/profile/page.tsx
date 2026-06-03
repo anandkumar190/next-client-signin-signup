7d53c089a20d96342ce4c5da5d87020a3a7ab90f
Anand <anandkumar.kumar190@gmail.com>
2026-06-03 12:50:49 +0530

    fix code

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -2,7 +2,6 @@
 import Link from "next/link";
 import {useRouter} from "next/navigation";
 import axios from "axios";
-import constants from "constants";
 import React,{ useState } from "react";
 
 export default function profile(){
