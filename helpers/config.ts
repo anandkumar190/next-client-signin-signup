f4440cc5f293da9d276cebad8916e5fd4479d4ca
Anand <63843910+anandkumar190@users.noreply.github.com>
2026-08-14 17:49:59 +0530

    domain updade

--- helpers/config.ts
+++ helpers/config.ts
@@ -1,1 +1,1 @@
-export const REMOTE_ASSET_BASE = process.env.NEXT_PUBLIC_REMOTE_ASSET_BASE || "http://13.207.126.237";
+export const REMOTE_ASSET_BASE = process.env.NEXT_PUBLIC_REMOTE_ASSET_BASE || "https://admin.urbanstylespace.com";
